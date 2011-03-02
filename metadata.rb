614890585423895e86fc3d2dc3dc10e634e8b35b
Darrin Eden <darrin.eden@gmail.com>
2011-03-01 21:49:37 -0800

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends  "python"
 depends  "apache2"
