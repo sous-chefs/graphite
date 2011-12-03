dc5113309785f6514f02bea561a387cceacd54e3
Darrin Eden <darrin.eden@gmail.com>
2011-12-02 17:26:14 -0800

    set graphite admin password script

---
+++ files/default/set_admin_passwd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# cribbed from https://gist.github.com/97863dc8171c7b473c94#file_set_admin_password.py
+
+import os,sys
+sys.path.append("/opt/graphite/webapp/graphite")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from django.contrib.auth.models import User
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+try:
+    u = User.objects.get(username__exact=username)
+    u.set_password(password)
+    u.save()
+except:
+    print "%s is not a user" % username
