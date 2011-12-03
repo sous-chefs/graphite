1948e4e3b50eaf6a7d2dbcabd06f348aa4ddc679
Darrin Eden <darrin.eden@gmail.com>
2011-12-02 17:31:52 -0800

    bump graphite cookbook version

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,8 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.5"
+version          "0.1.0"
 
 depends  "python"
 depends  "apache2"
-depends  "ganglia"
 supports "ubuntu"
