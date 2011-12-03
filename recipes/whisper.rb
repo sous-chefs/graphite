2341bf9d8cfce38585be084793067f95a9c76d97
Darrin Eden <darrin.eden@gmail.com>
2011-12-02 17:29:05 -0800

    tidy up

--- recipes/whisper.rb
+++ recipes/whisper.rb
@@ -1,16 +1,18 @@
-remote_file "/usr/src/whisper-#{node.graphite.version}.tar.gz" do
-  source node.graphite.whisper.uri
-  checksum node.graphite.whisper.checksum
+version = node[:graphite][:version]
+
+remote_file "/usr/src/whisper-#{version}.tar.gz" do
+  source node[:graphite][:whisper][:uri]
+  checksum node[:graphite][:whisper][:checksum]
 end
 
 execute "untar whisper" do
-  command "tar xzf whisper-#{node.graphite.version}.tar.gz"
-  creates "/usr/src/whisper-#{node.graphite.version}"
+  command "tar xzf whisper-#{version}.tar.gz"
+  creates "/usr/src/whisper-#{version}"
   cwd "/usr/src"
 end
 
 execute "install whisper" do
   command "python setup.py install"
-  creates "/usr/local/lib/python2.6/dist-packages/whisper-#{node.graphite.version}.egg-info"
-  cwd "/usr/src/whisper-#{node.graphite.version}"
+  creates "/usr/local/lib/python2.6/dist-packages/whisper-#{version}.egg-info"
+  cwd "/usr/src/whisper-#{version}"
 end
