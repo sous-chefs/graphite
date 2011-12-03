708ffac7fd45a3512d49b47cdddc6bd4e36f1809
Darrin Eden <darrin.eden@gmail.com>
2011-12-02 17:31:21 -0800

    default root graphite password attribute

--- attributes/graphite.rb
+++ attributes/graphite.rb
@@ -12,3 +12,5 @@ default[:graphite][:graphite_web][:checksum] = "cc78bab7fb26b"
 default[:graphite][:carbon][:line_receiver_interface] =   "127.0.0.1"
 default[:graphite][:carbon][:pickle_receiver_interface] = "127.0.0.1"
 default[:graphite][:carbon][:cache_query_interface] =     "127.0.0.1"
+
+default[:graphite][:password] = "change_me"
