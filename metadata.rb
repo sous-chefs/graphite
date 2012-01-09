5d2190880a385e7d581d7273c8dd11327c52912d
Darrin Eden <darrin.eden@gmail.com>
2012-01-09 14:37:43 -0800

    long description reference

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ maintainer       "Heavy Water Software Inc."
 maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.1"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.2"
 
 depends  "python"
 depends  "apache2"
