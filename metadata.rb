00b06a7123c50f00dfca69f8445e03ff946039aa
AJ Christensen <aj@junglist.gen.nz>
2012-01-20 15:08:55 +1300

    carbon-cache: runit support

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,10 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends  "python"
 depends  "apache2"
 supports "ubuntu"
+
+depends "runit"
