98bc7f3e5dfbd8756bdefb7717d1be6a1b5ecee1
AJ Christensen <aj@junglist.gen.nz>
2012-02-16 10:47:20 +1300

    Add runit support, tune storage schemas

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,14 @@
 maintainer       "Heavy Water Software Inc."
-maintainer_email "darrin@heavywater.ca"
+maintainer_email "ops@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.2.0"
+
+supports "ubuntu"
 
 depends  "python"
 depends  "apache2"
-supports "ubuntu"
-
 depends "runit"
+
+suggests "graphiti"
