28fab118fb7a1cca4e0389ccb96cd782d9d96e4e
Nathan L Smith <nlloyds@gmail.com>
2012-03-24 21:00:55 -0500

    Apache tweaks

--- recipes/web.rb
+++ recipes/web.rb
@@ -29,30 +29,26 @@ template "/etc/apache2/sites-available/graphite" do
   source "graphite-vhost.conf.erb"
 end
 
-apache_site "000-default" do
-  enable false
-end
-
 apache_site "graphite"
 
 directory "/opt/graphite/storage/log" do
-  owner "www-data"
-  group "www-data"
+  owner node['apache']['user']
+  group node['apache']['group']
 end
 
 directory "/opt/graphite/storage/log/webapp" do
-  owner "www-data"
-  group "www-data"
+  owner node['apache']['user']
+  group node['apache']['group']
 end
 
 directory "/opt/graphite/storage" do
-  owner "www-data"
-  group "www-data"
+  owner node['apache']['user']
+  group node['apache']['group']
 end
 
 directory "/opt/graphite/storage/whisper" do
-  owner "www-data"
-  group "www-data"
+  owner node['apache']['user']
+  group node['apache']['group']
 end
 
 cookbook_file "/opt/graphite/bin/set_admin_passwd.py" do
@@ -70,7 +66,7 @@ execute "set admin password" do
 end
 
 file "/opt/graphite/storage/graphite.db" do
-  owner "www-data"
-  group "www-data"
+  owner node['apache']['user']
+  group node['apache']['group']
   mode "644"
 end
