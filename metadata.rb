212a1a15b39eafa6616f8163c410d4f6af4e0873
Darrin Eden <darrin.eden@gmail.com>
2012-03-26 08:54:04 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer_email "ops@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 supports "ubuntu"
 
-depends  "python"
-depends  "apache2"
+depends "python"
+depends "apache2"
 depends "runit"
 
 suggests "graphiti"
