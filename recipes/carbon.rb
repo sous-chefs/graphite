4a1c15c7f4db11ea51de0f8bc09a6a14f143ee76
Xabier de Zuazo <xabier@onddo.com>
2013-03-24 03:28:43 +0100

    carbon config options passed as a hash to the carbon.conf template

--- recipes/carbon.rb
+++ recipes/carbon.rb
@@ -49,56 +49,8 @@ end
 template "#{node['graphite']['base_dir']}/conf/carbon.conf" do
   owner node['apache']['user']
   group node['apache']['group']
-  variables( # carbon-cache.py
-             :storage_dir => node['graphite']['storage_dir'],
-             :max_cache_size => node['graphite']['carbon']['max_cache_size'],
-             :max_updates_per_second => node['graphite']['carbon']['max_updates_per_second'],
-             :max_creates_per_second => node['graphite']['carbon']['max_creates_per_second'],
-             :line_receiver_interface => node['graphite']['carbon']['line_receiver_interface'],
-             :line_receiver_port => node['graphite']['carbon']['line_receiver_port'],
-             :enable_udp_listener => node['graphite']['carbon']['enable_udp_listener'],
-             :udp_receiver_interface => node['graphite']['carbon']['udp_receiver_interface'],
-             :udp_receiver_port => node['graphite']['carbon']['udp_receiver_port'],
-             :pickle_receiver_interface => node['graphite']['carbon']['pickle_receiver_interface'],
-             :pickle_receiver_port => node['graphite']['carbon']['pickle_receiver_port'],
-             :use_insecure_unpickler => node['graphite']['carbon']['use_insecure_unpickler'],
-             :cache_query_interface => node['graphite']['carbon']['cache_query_interface'],
-             :cache_query_port => node['graphite']['carbon']['cache_query_port'],
-             :use_flow_control => node['graphite']['carbon']['use_flow_control'],
-             :log_whisper_updates => node['graphite']['carbon']['log_whisper_updates'],
-             :whisper_autoflush => node['graphite']['carbon']['whisper_autoflush'],
-             :enable_amqp => node['graphite']['carbon']['enable_amqp'],
-             :amqp_host => node['graphite']['carbon']['amqp_host'],
-             :amqp_port => node['graphite']['carbon']['amqp_port'],
-             :amqp_vhost => node['graphite']['carbon']['amqp_vhost'],
-             :amqp_user => node['graphite']['carbon']['amqp_user'],
-             :amqp_password => node['graphite']['carbon']['amqp_password'],
-             :amqp_exchange => node['graphite']['carbon']['amqp_exchange'],
-             :amqp_metric_name_in_body => node['graphite']['carbon']['amqp_metric_name_in_body'],
-
-             # carbon-relay.py
-             :relay_line_receiver_interface => node['graphite']['carbon']['relay']['line_receiver_interface'],
-             :relay_line_receiver_port => node['graphite']['carbon']['relay']['line_receiver_port'],
-             :relay_pickle_receiver_interface => node['graphite']['carbon']['relay']['pickle_receiver_interface'],
-             :relay_pickle_receiver_port => node['graphite']['carbon']['relay']['pickle_receiver_port'],
-             :relay_method => node['graphite']['carbon']['relay']['relay_method'],
-             :relay_replication_factor => node['graphite']['carbon']['relay']['replication_factor'],
-             :relay_destinations => node['graphite']['carbon']['relay']['destinations'],
-             :relay_max_datapoints_per_message => node['graphite']['carbon']['relay']['max_datapoints_per_message'],
-             :relay_max_queue_size => node['graphite']['carbon']['relay']['max_queue_size'],
-             :relay_use_flow_control => node['graphite']['carbon']['relay']['use_flow_control'],
-
-             # carbon-aggregator.py
-             :aggregator_line_receiver_interface => node['graphite']['carbon']['aggregator']['line_receiver_interface'],
-             :aggregator_line_receiver_port => node['graphite']['carbon']['aggregator']['line_receiver_port'],
-             :aggregator_pickle_receiver_interface => node['graphite']['carbon']['aggregator']['pickle_receiver_interface'],
-             :aggregator_pickle_receiver_port => node['graphite']['carbon']['aggregator']['pickle_receiver_port'],
-             :aggregator_destinations => node['graphite']['carbon']['aggregator']['destinations'],
-             :aggregator_replication_factor => node['graphite']['carbon']['aggregator']['replication_factor'],
-             :aggregator_max_queue_size => node['graphite']['carbon']['aggregator']['max_queue_size'],
-             :aggregator_use_flow_control => node['graphite']['carbon']['aggregator']['use_flow_control'],
-             :aggregator_max_datapoints_per_message => node['graphite']['carbon']['aggregator']['max_datapoints_per_message'],
-             :aggregator_max_aggregation_intervals => node['graphite']['carbon']['aggregator']['max_aggregation_intervals']
+  variables( :storage_dir => node['graphite']['storage_dir'],
+             :carbon_options => node['graphite']['carbon']
   )
 end
 
