22215b77434d891e17dcea79120ae55b8961bd1f
Tim Smith <tsmith@llnw.com>
2013-04-23 22:05:13 -0700

    Add admin e-mail to the vhost via attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,6 +23,7 @@ default['graphite']['whisper']['checksum'] = "36b5fa917526224678da0a530a6f276d00
 
 default['graphite']['graphite_web']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
 default['graphite']['graphite_web']['checksum'] = "4fd1d16cac3980fddc09dbf0a72243c7ae32444903258e1b65e28428a48948be"
+default['graphite']['graphite_web']['admin_email'] = "admin@org.com"
 
 default['graphite']['carbon']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/carbon-#{node['graphite']['version']}.tar.gz"
 default['graphite']['carbon']['checksum'] = "4f37e00595b5b078edb9b3f5cae318f752f4446a82623ea4da97dd7d0f6a5072"
