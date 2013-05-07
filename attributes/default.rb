2c55ad10f5ce1d721f7f353f55eea73a218f9506
Sergey Khaladzinski <gansbrest@yahoo.com>
2013-05-07 16:04:43 -0400

    Added basic auth support for apache

--- attributes/default.rb
+++ attributes/default.rb
@@ -76,3 +76,8 @@ when "debian"
 else
   default['graphite']['uwsgi_packages'] = []
 end
+
+default['graphite']['apache']['basic_auth'] = false
+default['graphite']['apache']['basic_auth']['file_path'] = "#{node['graphite']['doc_root']}/htpasswd"
+default['graphite']['apache']['basic_auth']['user'] = nil
+default['graphite']['apache']['basic_auth']['pass'] = nil
--- recipes/apache.rb
+++ recipes/apache.rb
@@ -5,6 +5,12 @@ if node['graphite']['ssl']['enabled']
   include_recipe "apache2::mod_ssl"
 end
 
+if node['graphite']['apache']['basic_auth']
+  execute "create apache basic_auth file for graphite" do
+    command "htpasswd -bc #{node['graphite']['apache']['basic_auth']['file_path']} #{node['graphite']['apache']['basic_auth']['user']} #{node['graphite']['apache']['basic_auth']['pass']}"
+  end
+end
+
 template "#{node['apache']['dir']}/sites-available/graphite" do
   source "graphite-vhost.conf.erb"
 end
