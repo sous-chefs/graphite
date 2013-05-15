ac5066fde1c1d04445246051227d52fe0552829e
Tim Smith <tsmith@llnw.com>
2013-05-15 09:40:01 -0700

    Apache should be reloaded not restarted

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -13,7 +13,7 @@ end
 
 template "#{node['apache']['dir']}/sites-available/graphite" do
   source "graphite-vhost.conf.erb"
-  notifies :restart, "service[apache2]"
+  notifies :reload, "service[apache2]"
 end
 
 apache_site "graphite"
