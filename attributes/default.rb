675c3f4db99d980819111db26483b83b892200ca
Xabier de Zuazo <xabier@onddo.com>
2013-05-16 02:24:53 +0200

    some attributes sorted, added graphite-web section

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,10 +8,6 @@ default['graphite']['password'] = "change_me"
 default['graphite']['url'] = "graphite"
 default['graphite']['url_aliases'] = []
 default['graphite']['listen_port'] = 80
-default['graphite']['ssl']['enabled'] = false
-default['graphite']['ssl']['cipher_suite'] = "ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP"
-default['graphite']['ssl']['certificate_file'] = "/etc/ssl/server.crt"
-default['graphite']['ssl']['certificate_key_file'] = "/etc/ssl/server.key"
 default['graphite']['base_dir'] = "/opt/graphite"
 default['graphite']['doc_root'] = "/opt/graphite/webapp"
 default['graphite']['storage_dir'] = "/opt/graphite/storage"
@@ -21,13 +17,10 @@ default['graphite']['django_root'] = "@DJANGO_ROOT@"
 default['graphite']['whisper']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/whisper-#{node['graphite']['version']}.tar.gz"
 default['graphite']['whisper']['checksum'] = "36b5fa917526224678da0a530a6f276d00074f0aa98acd6e2412c79521f9c4ff"
 
-default['graphite']['graphite_web']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
-default['graphite']['graphite_web']['checksum'] = "4fd1d16cac3980fddc09dbf0a72243c7ae32444903258e1b65e28428a48948be"
-default['graphite']['graphite_web']['debug'] = "False"
-default['graphite']['graphite_web']['admin_email'] = "admin@org.com"
-
 default['graphite']['carbon']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/carbon-#{node['graphite']['version']}.tar.gz"
 default['graphite']['carbon']['checksum'] = "4f37e00595b5b078edb9b3f5cae318f752f4446a82623ea4da97dd7d0f6a5072"
+
+default['graphite']['encrypted_data_bag']['name'] = nil
 
 # carbon-cache.py
 default['graphite']['carbon']['line_receiver_interface'] =   "0.0.0.0"
@@ -47,7 +40,6 @@ default['graphite']['carbon']['max_updates_per_second'] = "1000"
 default['graphite']['carbon']['log_whisper_updates'] = "False"
 default['graphite']['carbon']['whisper_autoflush'] = "False"
 
-default['graphite']['storage_aggregation'] = nil
 default['graphite']['storage_schemas'] = [
   {
     'name' => 'catchall', 
@@ -99,8 +91,13 @@ default['graphite']['carbon']['aggregator']['use_flow_control'] = "True"
 default['graphite']['carbon']['aggregator']['max_datapoints_per_message'] = 500
 default['graphite']['carbon']['aggregator']['max_aggregation_intervals'] = 5
 
+default['graphite']['storage_aggregation'] = nil
 
-default['graphite']['encrypted_data_bag']['name'] = nil
+# graphite-web
+default['graphite']['graphite_web']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
+default['graphite']['graphite_web']['checksum'] = "4fd1d16cac3980fddc09dbf0a72243c7ae32444903258e1b65e28428a48948be"
+default['graphite']['graphite_web']['debug'] = "False"
+default['graphite']['graphite_web']['admin_email'] = "admin@org.com"
 
 default['graphite']['web_server'] = 'apache'
 default['graphite']['user_account'] = node['apache']['user']
@@ -113,6 +110,11 @@ when "debian"
 else
   default['graphite']['uwsgi_packages'] = []
 end
+
+default['graphite']['ssl']['enabled'] = false
+default['graphite']['ssl']['cipher_suite'] = "ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv2:+EXP"
+default['graphite']['ssl']['certificate_file'] = "/etc/ssl/server.crt"
+default['graphite']['ssl']['certificate_key_file'] = "/etc/ssl/server.key"
 
 default['graphite']['apache']['basic_auth']['enabled'] = false
 default['graphite']['apache']['basic_auth']['file_path'] = "#{node['graphite']['doc_root']}/htpasswd"
