ce416ad3da6d9ae3a5314012ff6b929ff5dbc6bb
Sean Escriva <sean.escriva@gmail.com>
2014-01-15 22:57:16 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@hw-ops.com'
 license          'Apache 2.0'
 description      'Installs/Configures graphite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.4'
+version          '0.4.5'
 
 supports 'ubuntu'
 supports 'debian'
