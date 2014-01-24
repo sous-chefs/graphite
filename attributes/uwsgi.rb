6f4dbeb22e025fb0cfaed8e63926f150c3b71e66
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2014-01-24 15:20:21 -0300

    added service_type attribute

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -2,3 +2,4 @@ default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = true
 default['graphite']['uwsgi']['listen_http'] = true
+default['graphite']['uwsgi']['service_type'] = 'runit'
