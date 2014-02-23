b972949aab5fc4dcf9076f26b0de596b29d1fe1d
Sean Escriva <sean.escriva@gmail.com>
2014-02-22 23:19:23 -0800

    add composable recipe for directory management

---
+++ spec/recipes/_directories_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'graphite::_directories' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:storage) { "/opt/graphite/storage" }
+
+  it "creates the graphite storage directory" do
+    expect(chef_run).to create_directory(storage).with(
+      user: 'graphite',
+      group: 'graphite',
+      recursive: true,
+    )
+  end
+
+  %w{ log whisper rrd}.each do |dir|
+    it "creates the graphite storage subdirectory #{dir}" do
+      expect(chef_run).to create_directory(File.join(storage, dir)).with(
+        user: 'graphite',
+        group: 'graphite',
+        recursive: true,
+      )
+    end
+  end
+
+end
