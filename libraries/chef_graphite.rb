e79b12d7d9de5d38c87fcb9a8210f72516da9940
Sean Escriva <sean.escriva@gmail.com>
2014-03-03 16:15:36 -0800

    accumulator heavyweight for handling carbon.conf

---
+++ libraries/chef_graphite.rb
@@ -0,0 +1,16 @@
+module ChefGraphite
+  class << self
+
+    def ini_file(resources)
+      # TODO return string constant
+    end
+
+    private
+
+    def generate_conf_data(resources)
+      # TODO return a ruby hash
+      # each key corresponds to ini file section
+    end
+
+  end
+end
