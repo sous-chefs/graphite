d54c308604550552584a3ba24e95066880cba157
Sean Escriva <sean.escriva@gmail.com>
2014-03-05 14:31:37 -0800

    updated carbon recipe using composable recipes

--- recipes/carbon.rb
+++ recipes/carbon.rb
@@ -20,70 +20,7 @@
 include_recipe 'python'
 include_recipe 'python::pip'
 
-if node['graphite']['carbon']['enable_amqp']
-
-  python_pip 'txamqp' do
-    action :install
-  end
-
-  amqp_password = node['graphite']['carbon']['amqp_password']
-  if node['graphite']['encrypted_data_bag']['name']
-    data_bag_name = node['graphite']['encrypted_data_bag']['name']
-    data_bag_item = Chef::EncryptedDataBagItem.load(data_bag_name, 'graphite')
-    amqp_password = data_bag_item['amqp_password']
-  else
-    Chef::Log.warn 'This recipe uses encrypted data bags for carbon AMQP password but no encrypted data bag name is specified - fallback to node attribute.'
-  end
-
-end
-
-# sadly, have to pin Twisted to known good version
-# install before carbon so it's used
-python_pip 'Twisted' do
-  version lazy { node['graphite']['twisted_version'] }
-end
-
-python_pip 'carbon' do
-  package_name lazy {
-    node['graphite']['package_names']['carbon'][node['graphite']['install_type']]
-  }
-  version lazy {
-    node['graphite']['install_type'] == 'package' ? node['graphite']['version'] : nil
-  }
-end
-
-directory "#{node['graphite']['base_dir']}/conf" do
-  owner node['graphite']['user_account']
-  group node['graphite']['group_account']
-  recursive true
-end
-
-template "#{node['graphite']['base_dir']}/conf/carbon.conf" do
-  owner node['graphite']['user_account']
-  group node['graphite']['group_account']
-  carbon_options = node['graphite']['carbon'].dup
-  carbon_options['amqp_password'] = amqp_password unless amqp_password.nil?
-  variables(
-    :storage_dir => node['graphite']['storage_dir'],
-    :carbon_options => carbon_options
-  )
-end
-
-directory node['graphite']['storage_dir'] do
-  owner node['graphite']['user_account']
-  group node['graphite']['group_account']
-  recursive true
-end
-
-%w{ log whisper rrd }.each do |dir|
-  directory "#{node['graphite']['storage_dir']}/#{dir}" do
-    owner node['graphite']['user_account']
-    group node['graphite']['group_account']
-  end
-end
-
-directory "#{node['graphite']['base_dir']}/lib/twisted/plugins/" do
-  owner node['graphite']['user_account']
-  group node['graphite']['group_account']
-  recursive true
-end
+include_recipe "graphite::_user"
+include_recipe "graphite::_carbon_packages"
+include_recipe "graphite::_directories"
+include_recipe "graphite::_carbon_config"
