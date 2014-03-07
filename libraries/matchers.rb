a4bf8dff149cbdfc2c930399783ac3bc03ef2480
Sean Escriva <sean.escriva@gmail.com>
2014-03-07 15:05:17 -0800

    Move new matchers to correct location

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,6 +1,15 @@
-# Matchers for chefspec 3
-
 if defined?(ChefSpec)
+  def create_graphite_storage(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:graphite_storage, :create, name)
+  end
+
+  def delete_graphite_storage(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:graphite_storage, :delete, name)
+  end
+
+  def create_graphite_carbon_conf_accumulator(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:graphite_carbon_conf_accumulator, :create, name)
+
   def enable_runit_service(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:runit_service, :enable, resource_name)
   end
