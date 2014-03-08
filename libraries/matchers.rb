a3b661e50cce2fa4081467fab97fc9e1116c6bb3
Sean Escriva <sean.escriva@gmail.com>
2014-03-07 16:15:08 -0800

    fix: close block in matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -9,6 +9,7 @@ if defined?(ChefSpec)
 
   def create_graphite_carbon_conf_accumulator(name)
     ChefSpec::Matchers::ResourceMatcher.new(:graphite_carbon_conf_accumulator, :create, name)
+  end
 
   def enable_runit_service(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:runit_service, :enable, resource_name)
