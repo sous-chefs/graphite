5e7a87308b64d39929bfb6c087cd6662d1ccdb4e
Sean Escriva <sean.escriva@gmail.com>
2014-03-08 00:29:20 -0800

    add resource and provider loading to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'chefspec'
 require 'chefspec/librarian'
+require 'chef/mixin/convert_to_class_name'
 
 RSpec.configure do |config|
   config.log_level = :fatal
@@ -7,4 +8,34 @@ end
 
 if ENV['COVERAGE']
   at_exit { ChefSpec::Coverage.report! }
+end
+
+def load_resource(cookbook, lwrp)
+  require "chef/resource/lwrp_base"
+  name = class_name_for_lwrp(cookbook, lwrp)
+  unless Chef::Resource.const_defined?(name)
+    Chef::Resource::LWRPBase.build_from_file(
+      cookbook,
+      File.join(File.dirname(__FILE__), %W{.. resources #{lwrp}.rb}),
+      nil
+    )
+  end
+end
+
+def load_provider(cookbook, lwrp)
+  require "chef/provider/lwrp_base"
+  name = class_name_for_lwrp(cookbook, lwrp)
+  unless Chef::Provider.const_defined?(name)
+    Chef::Provider::LWRPBase.build_from_file(
+      cookbook,
+      File.join(File.dirname(__FILE__), %W{.. providers #{lwrp}.rb}),
+      nil
+    )
+  end
+end
+
+def class_name_for_lwrp(cookbook, lwrp)
+  Chef::Mixin::ConvertToClassName.convert_to_class_name(
+    Chef::Mixin::ConvertToClassName.filename_to_qualified_string(cookbook, lwrp)
+  )
 end
