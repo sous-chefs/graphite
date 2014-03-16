701a0ad64337d4e83a31f3e36e7fc4645bf4573e
Fletcher Nichol <fnichol@nichol.ca>
2014-03-16 00:32:53 -0600

    Add specs for carbon_relay & carbon_aggregator resources & providers.

--- providers/carbon_aggregator.rb
+++ providers/carbon_aggregator.rb
@@ -23,4 +23,8 @@ end
 
 use_inline_resources
 
-action :create do; end
+action :create do
+end
+
+action :delete do
+end
---
+++ spec/resources/carbon_relay_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+load_resource("graphite", "carbon_relay")
+
+describe Chef::Resource::GraphiteCarbonRelay do
+
+  let(:resource_name) { "echo" }
+
+  it "sets the default attribute to name" do
+    expect(resource.name).to eq("echo")
+  end
+
+  it "attribute config defaults to nil" do
+    expect(resource.config).to be_nil
+  end
+
+  it "attribute config takes a Hash value" do
+    resource.config("a" => "metaphor")
+
+    expect(resource.config).to eq("a" => "metaphor")
+  end
+
+  it "action defaults to :create" do
+    expect(resource.action).to eq(:create)
+  end
+
+  it "actions include :delete" do
+    expect(resource.allowed_actions).to include(:delete)
+  end
+end
