92c7cdcafedea15c5cccd2a9cde115489a03d616
Ken Robertson <ken@apcera.com>
2014-08-11 17:21:43 -0700

    Increase uwsgi runit timeout

--- recipes/uwsgi_runit.rb
+++ recipes/uwsgi_runit.rb
@@ -3,6 +3,6 @@ include_recipe 'runit'
 runit_service 'graphite-web' do
   # uwsgi/graphite-web can sometimes be a tad slow to startup, so allow more
   # than the default 7 seconds for it load up.
-  sv_timeout 15
+  sv_timeout 30
   default_logger true
 end
