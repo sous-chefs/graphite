7e27bd56876a4edb4dd4988feab6c0b333810bf8
msshin <msshin@enswer.net>
2014-08-22 22:38:18 +0900

    Changed graphite site file name in apache

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -7,12 +7,12 @@ execute 'create apache basic_auth file for graphite' do
   only_if { node['graphite']['apache']['basic_auth']['enabled'] }
 end
 
-template "#{node['apache']['dir']}/sites-available/graphite" do
+template "#{node['apache']['dir']}/sites-available/graphite.conf" do
   source 'graphite-vhost.conf.erb'
   notifies :reload, 'service[apache2]'
 end
 
-apache_site 'graphite'
+apache_site 'graphite.conf'
 
 apache_site '000-default' do
   enable false
