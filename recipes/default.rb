9e15283eb6808e5523acdf63c1bb5d8258cf56d3
Sean Escriva <sean.escriva@gmail.com>
2014-08-27 23:26:19 -0400

    Library cookbook should have no-op default recipe
    allows for safe inclusion in users custom wrappers

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-include_recipe 'graphite::carbon'
-include_recipe 'graphite::carbon_cache'
-include_recipe 'graphite::web'
