3799c1cf3100d75d572560d7bed18f6ee5f146f2
Sean Escriva <sean.escriva@gmail.com>
2014-08-27 23:25:40 -0400

    Adjust copyright date

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: graphite
 # Recipe:: default
 #
-# Copyright 2011, Heavy Water Software Inc.
+# Copyright 2014, Heavy Water Software Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
