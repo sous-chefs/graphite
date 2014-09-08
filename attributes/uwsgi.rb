361899ec9983666a0e9b1bccb794c3e04ad53b8f
Sean Escriva <sean.escriva@gmail.com>
2014-09-07 22:26:58 -0400

    Move port attribute for uwsgi, fixes #190
    This attribute is probably better grouped with other uwsgi settings

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -2,4 +2,5 @@ default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
 default['graphite']['uwsgi']['listen_http'] = false
+default['graphite']['uwsgi']['port'] = 8080
 default['graphite']['uwsgi']['service_type'] = 'runit'
