3653abaa97da46141ab7131ed74a9ee7307baa1b
Josh Ellithorpe <quest@mac.com>
2014-09-20 23:28:27 -0700

    Use 60 second timeout for graphite, this works

--- recipes/uwsgi_runit.rb
+++ recipes/uwsgi_runit.rb
@@ -3,7 +3,6 @@ include_recipe 'runit'
 runit_service 'graphite-web' do
   # uwsgi/graphite-web can sometimes be a tad slow to startup, so allow more
   # than the default 7 seconds for it load up.
-  sv_timeout 30
   default_logger true
-  sv_timeout 30
+  sv_timeout 60
 end
