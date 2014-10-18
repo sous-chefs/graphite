b1e3d5077d7e25f76c99e9b57e3d76f6990e7612
Andrew Goktepe <andrew.goktepe@savantsystems.com>
2014-10-17 18:41:40 -0700

    use chmod-socket option in uwsgi so it can be overridden by a wrapper cookbook to allow access from the proxying web server

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -1,4 +1,5 @@
 default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+default['graphite']['uwsgi']['socket_permissions'] = '755'
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
 default['graphite']['uwsgi']['listen_http'] = false
