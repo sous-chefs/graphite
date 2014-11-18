0990c9d73f8f5dea8cb3f70c5dbbb776982cd7cb
Cameron Johnston <cameron@rootdown.net>
2014-11-17 20:40:49 -0700

    bump version to 1.0.3 for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@hw-ops.com'
 license          'Apache 2.0'
 description      'Installs/Configures graphite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 supports 'ubuntu'
 supports 'debian'
