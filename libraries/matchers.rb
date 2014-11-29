08cbf29f99296ad23c42275d58562793834d7449
Sean Escriva <sean.escriva@gmail.com>
2014-11-28 22:33:26 -0500

    Correct license header for libraries

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: graphite
+# Library:: ChefSpec matchers
+#
+# Copyright 2014, Heavy Water Ops, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 if defined?(ChefSpec)
   def create_graphite_storage(name)
     ChefSpec::Matchers::ResourceMatcher.new(:graphite_storage, :create, name)
