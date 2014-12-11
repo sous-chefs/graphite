2ca3c319ef478d5ec80f9a27bc88a83a0262d0f2
Mitsutoshi Aoe <maoe@foldr.in>
2014-12-11 15:17:06 +0900

    Add support for --chmod-socket option in uwsgi

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -1,4 +1,5 @@
 default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+default['graphite']['uwsgi']['chmod_socket'] = nil # '664' for example
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
 default['graphite']['uwsgi']['listen_http'] = false
