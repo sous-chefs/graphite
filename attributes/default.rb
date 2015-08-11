ef0daf753c0565084a404736ce0c902f2349a779
Tim Smith <tim@cozy.co>
2015-08-10 17:40:28 -0700

    Use the complete Twisted version number

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@
 #
 
 default['graphite']['version'] = '0.9.12'
-default['graphite']['twisted_version'] = '13.1'
+default['graphite']['twisted_version'] = '13.1.0'
 default['graphite']['django_version'] = '1.5.5'
 default['graphite']['password'] = 'change_me'
 default['graphite']['user'] = 'graphite'
