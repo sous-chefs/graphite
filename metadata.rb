30c2c6a9d332e576f7f1b90a4e26f772d1012f09
Tim Smith <tsmith@chef.io>
2016-10-11 01:16:37 +0100

    Add chef_version to metadata

--- metadata.rb
+++ metadata.rb
@@ -26,3 +26,4 @@ suggests 'delayed_evaluator'
 
 source_url 'https://github.com/hw-cookbooks/graphite' if respond_to?(:source_url)
 issues_url 'https://github.com/hw-cookbooks/graphite/issues' if respond_to?(:issues_url)
+chef_version '>= 11.0' if respond_to?(:chef_version)
