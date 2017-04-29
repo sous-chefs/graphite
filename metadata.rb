e972dd15f4c34ed5f4518a5b4054529b25b11307
Dan Webb <dan.webb@damacus.io>
2017-04-29 09:15:33 +0100

    Update LICENSE in metdata to be correct (#281)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'graphite'
 maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures graphite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.5'
