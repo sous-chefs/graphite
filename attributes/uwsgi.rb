e34bbb89dda230d1853dfba7bd7d889f4b36b82f
Akshay Baheti <akshay@apcera.com>
2017-07-31 11:32:50 -0700

    Updating uwsgi socket for centos.

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -1,4 +1,8 @@
-default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+if ['centos', 'redhat'].include?(node[:platform]) 
+  default['graphite']['uwsgi']['socket'] = '/var/run/uwsgi/uwsgi.sock'
+else
+  default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+end
 default['graphite']['uwsgi']['workers'] = 8
 default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
 default['graphite']['uwsgi']['listen_http'] = false
