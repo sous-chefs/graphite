5495f9e7f762a53a6c4a959627f0c98e7846a7be
Earl C. Ruby III <earl@apcera.com>
2017-08-17 17:51:29 -0700

    graphite-web startup failure
    Sometimes graphite-web starts, but runit fails to understand that it started,
    so timeout the startup after 10 seconds but try to start it at least 10 times.

--- recipes/uwsgi_runit.rb
+++ recipes/uwsgi_runit.rb
@@ -1,9 +1,11 @@
 include_recipe 'runit'
 
 runit_service 'graphite-web' do
-  # uwsgi/graphite-web can sometimes be a tad slow to startup, so allow more
-  # than the default 7 seconds for it load up.
-  sv_timeout 120
+  # Sometimes graphite-web starts, but runit fails to understand that it started,
+  # so timeout the startup after 10 seconds but try to start it at least 10 times.
+  sv_timeout 10
+  retries 10
+  retry_delay 2
   default_logger true
   restart_on_update false
 end
