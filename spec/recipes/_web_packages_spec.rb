4e58902d7fb1f22662cb78a7eaa1ca71e73e0a09
Tim Smith <tsmith@chef.io>
2018-06-22 12:42:22 -0600

    Delete resource specs and fix a few others
    Closer to getting there
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/recipes/_web_packages_spec.rb
+++ spec/recipes/_web_packages_spec.rb
@@ -4,7 +4,7 @@ describe 'graphite::_web_packages' do
   context 'on centos' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(
-        platform: 'centos', version: '7.3'
+        platform: 'centos', version: '7.4.1708'
       ).converge(described_recipe)
     end
 
@@ -14,14 +14,14 @@ describe 'graphite::_web_packages' do
   end
 
   it 'installs system packages from an attribute' do
-    node.set['graphite']['system_packages'] = %w(foo bar)
+    node.override['graphite']['system_packages'] = %w(foo bar)
 
     expect(chef_run).to install_package('foo')
     expect(chef_run).to install_package('bar')
   end
 
   it 'installs a specific version of the django pip package' do
-    node.set['graphite']['django_version'] = 'epoch'
+    node.override['graphite']['django_version'] = 'epoch'
 
     expect(chef_run).to install_python_pip('django').with(
       version: 'epoch'
@@ -35,8 +35,8 @@ describe 'graphite::_web_packages' do
   end
 
   it 'installs a specific version of graphite_web from a package' do
-    node.set['graphite']['install_type'] = 'package'
-    node.set['graphite']['version'] = '9.8.7'
+    node.override['graphite']['install_type'] = 'package'
+    node.override['graphite']['version'] = '9.8.7'
 
     expect(chef_run).to install_python_pip('graphite_web').with(
       package_name: 'graphite-web',
@@ -45,8 +45,8 @@ describe 'graphite::_web_packages' do
   end
 
   it 'installs graphite_web from a url source' do
-    node.set['graphite']['install_type'] = 'source'
-    node.set['graphite']['package_names']['graphite_web']['source'] = 'http://yep'
+    node.override['graphite']['install_type'] = 'source'
+    node.override['graphite']['package_names']['graphite_web']['source'] = 'http://yep'
 
     expect(chef_run).to install_python_pip('graphite_web').with(
       package_name: 'http://yep',
--- spec/recipes/carbon_spec.rb
+++ spec/recipes/carbon_spec.rb
@@ -3,12 +3,6 @@ require 'spec_helper'
 describe 'graphite::carbon' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe) }
 
-  %w(python python::pip).each do |r|
-    it "includes the external #{r} recipe" do
-      expect(chef_run).to include_recipe(r.to_s)
-    end
-  end
-
   %w(_user _carbon_packages _directories _carbon_config).each do |g|
     it "includes the internal graphite::#{g} recipe" do
       expect(chef_run).to include_recipe("graphite::#{g}")
