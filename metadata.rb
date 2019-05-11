27a9c867da740b363477494c9a65cd2367e22dce
Oscar Mendoza <chi23bears@gmail.com>
2019-05-11 05:22:28 -0500

    Update carbon_relay.rb (#310)
    * Update carbon_relay.rb
    
    * Update metadata.rb
    
    * Update CHANGELOG.md

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs/Configures graphite'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.5'
+version          '1.0.6'
 
 supports 'ubuntu'
 supports 'debian'
