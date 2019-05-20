92edad72965009a2fa26991922390b22b443a8a3
Xorima <4923914+Xorima@users.noreply.github.com>
2019-05-20 20:42:46 +0100

    Standardise Danger (#314)
    * Standardise Danger
    
    This commit will standardise the dangerfile and bring it in line with all other repositories
    
    * Cookstyle
    
    * Update Orb to 2x
    
    * Update stage name

--- spec/libraries/chef_graphite_python_spec.rb
+++ spec/libraries/chef_graphite_python_spec.rb
@@ -48,7 +48,7 @@ describe ChefGraphite::PythonWriter do
           flushrrdcached = 'unix:/var/run/rrdcached.sock'
           whisper_dir = '/opt/graphite/storage/whisper'
           ldap_base_user = 'CN=some_readonly_account,DC=mycompany,DC=com'
-       EOF
+      EOF
     end
 
     it 'takes a hash and writes the python config with uppercase root keys' do
@@ -74,7 +74,7 @@ describe ChefGraphite::PythonWriter do
           FLUSHRRDCACHED = 'unix:/var/run/rrdcached.sock'
           WHISPER_DIR = '/opt/graphite/storage/whisper'
           LDAP_BASE_USER = 'CN=some_readonly_account,DC=mycompany,DC=com'
-       EOF
+      EOF
     end
   end
   describe '#pythonize' do
@@ -134,9 +134,9 @@ describe ChefGraphite::PythonWriter do
                'tap' => { purple: 'pants' },
                'jazzercise' => false,
                'icedance' => 1
-      )).to eq(
-        "{'jazzhands': ['tap', True, 100], 'tap': {'purple': 'pants'}, 'jazzercise': False, 'icedance': 1}"
-      )
+             )).to eq(
+               "{'jazzhands': ['tap', True, 100], 'tap': {'purple': 'pants'}, 'jazzercise': False, 'icedance': 1}"
+             )
     end
   end
 end
